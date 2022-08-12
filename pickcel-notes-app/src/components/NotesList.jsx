c54648930fe552973abdb8bef86060a7c5ee85fa
Madhavi-Pasupuleti <95956493+Madhavi-Pasupuleti@users.noreply.github.com>
2022-08-12 09:57:59 +0530

    changed notes list UI

--- pickcel-notes-app/src/components/NotesList.jsx
+++ pickcel-notes-app/src/components/NotesList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import {Table, Button} from 'react-bootstrap';
+import {Button, Accordion} from 'react-bootstrap';
 import crud_operations from '../crud/crud_operations';
+import "../noteslist.css"
 
 function NotesList({ getNotesId }) {
     const [notesNtitle, setNotesNTitle] = useState([]);
@@ -25,34 +26,33 @@ function NotesList({ getNotesId }) {
 
 
     return (
-        <div>
-            <Button variant="info" onClick={getNotes}>Refresh</Button>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Title</th>
-                        <th>Notes</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {notesNtitle.map((doc,i) => {
-                        return (
-                            <tr key={doc.id}>
-                                <td>{i+1}</td>
-                                <td>{doc.title}</td>
-                                <td>{doc.startnotes}</td>
-                                <td>
-                                    <Button variant='secondary' onClick={(e) => getNotesId(doc.id)}>Update</Button>
-                                    <Button variant="danger" onClick={(e) => handleDelete(doc.id)}>Delete</Button>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                   
-                </tbody>
-            </Table>
-        </div>
+        <div className='container'>
+            <div className='refresh'>
+                <Button className='refreshbtn' onClick={getNotes}>Refresh</Button>
+            </div>
+            <div className='accodian'> 
+                {notesNtitle.map((doc,i) => {
+                    return(
+                        <Accordion key={doc.id} >
+                            <Accordion.Item eventKey={i}>
+                                <Accordion.Header>
+                                    <h5> {doc.title}</h5>
+                                </Accordion.Header>
+                                <Accordion.Body className='body'>
+                                    <div className='notes'>
+                                    {doc.startnotes}
+                                    </div>
+                                    <div className='buttons'>
+                                        <Button variant='secondary' onClick={(e) => getNotesId(doc.id)}>Update</Button>
+                                        <Button variant="danger" onClick={(e) => handleDelete(doc.id)}>Delete</Button>
+                                    </div>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    )
+                })}
+            </div>
+       </div>
     )
 }
 
